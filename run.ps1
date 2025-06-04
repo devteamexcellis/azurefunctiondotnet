3edb4c3bd94c30749db83016c7a5ef4a6800beb4
roberto abellon <rabellon@excelliscorp.com>
2025-06-04 08:37:41 +0800

    first commit

---
+++ run.ps1
@@ -0,0 +1,9 @@
+# Kill any leftover function host process
+taskkill /f /im func.exe > $null 2>&1
+
+# Clean up previous run artifacts
+Remove-Item -Force -Recurse .\bin\Debug\net8.0\.azurefunctions -ErrorAction SilentlyContinue
+Remove-Item -Force -Recurse .\obj\Debug\net8.0\WorkerExtensions -ErrorAction SilentlyContinue
+
+# Start the function
+func start
